4edee707f167868488c573d11a34313b311958db
MarsBroshok96 <marsbroshok1996@gmail.com>
2022-12-09 19:11:41 +0300

    Test re-push

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -4,14 +4,14 @@ Program returns differences between file1 and file2
 
 
 from gendiff.cli import parse_args
-from gendiff import engine
+from gendiff.engine import generate_diff
 
 
 def main():
     """Name as main"""
     path_file1, path_file2, format_name = parse_args()
 
-    print(engine.generate_diff(path_file1, path_file2, format_name))
+    print(generate_diff(path_file1, path_file2, format_name))
 
 
 if __name__ == '__main__':
